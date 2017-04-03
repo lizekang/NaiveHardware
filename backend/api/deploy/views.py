f54dc83051f1c328f6482a3753bf2161836b99dd
lizekang <zekangli@hustunique.com>
2017-04-04 03:06:47 +0800

    mix appjson and packagejson

--- backend/api/deploy/views.py
+++ backend/api/deploy/views.py
@@ -3,6 +3,9 @@ import string
 import random
 import httplib2
 
+from urllib.request import urlopen
+from urllib.error import HTTPError
+
 import tornado.httpclient
 from tornado import gen
 from tornado.httpclient import (
@@ -33,12 +36,14 @@ class DeployHandler(base.APIBaseHandler):
                                   uid=uuid)
         self.set_run(project)
         task_list = self.get_task_list(self.current_user.projects)
+        app_json_obj = self.generate_config(self.get_config()).generate_app_json()
+        package_json_obj = self.generate_config(self.get_config()).generate_package_json()
         headers = {'Content-type': "application/json"}
         conn = httplib2.Http()
-        url = "http://" + server_ip + "/task"
-        resp, content = conn.request(url, 'POST',
+        server_url = "http://" + server_ip + "/task"
+        resp, content = conn.request(server_url, 'POST',
                                      headers=headers,
-                                     body=json.dumps([task_list, {"device_ip": device_ip}]))
+                                     body=json.dumps([task_list, {"device_ip": device_ip}, app_json_obj, package_json_obj]))
         if resp['status'] == '200':
             self.finish(json.dumps({"errno": True, "data": task_list}))
         else:
@@ -48,8 +53,8 @@ class DeployHandler(base.APIBaseHandler):
     def set_run(self, project):
         project.is_run = True
 
-    @base.db_success_or_pass
-    def get_task_list(self, projects):
+    @staticmethod
+    def get_task_list(projects):
         task_list = list()
         for project in projects:
             if project.is_run is True:
@@ -65,3 +70,83 @@ class DeployHandler(base.APIBaseHandler):
                 }
                 task_list.append(task)
         return task_list
+
+    @base.db_success_or_pass
+    def get_config(self):
+        objects = list()
+        for sensor in self.current_user.sensors:
+            a_object = {
+                "id": sensor.id,
+                "driver": sensor.type
+            }
+            objects.append(a_object)
+        for effector in self.current_user.effectors:
+            a_object = {
+                "id": effector.id,
+                "driver": effector.type
+            }
+            objects.append(a_object)
+        return objects
+
+    @staticmethod
+    def generate_config(objects):
+        obj = RuffJson(json.dumps(objects))
+        return obj
+
+
+class RuffJson(object):
+    def __init__(self, json):
+        # when init you should pass the command.json
+        self.__command_json = json
+        self.path = '/home/lzk/ruff/cc2531/ruff_modules/'
+
+    def set_path(self, path):
+        self.path = path
+
+    def get_command_json(self):
+        # get the command json
+        return self.__command_json
+
+    def generate_app_json(self):  # generate the app.json
+        command_list = json.loads(self.__command_json)
+        app_json_list = []
+        for task in command_list:
+            print(task['driver'])
+            try:
+                html = urlopen('https://raw.githubusercontent.com/ruff-drivers/'+task['driver']+'/master/driver.json')
+                json_str = html.read().decode('utf-8')
+                json_ob = json.loads(json_str)
+            except HTTPError as e:
+                print(e)
+
+            for i in list(json_ob.keys()):
+                if i == 'models':
+                    task[i] = (json_ob[i][0] if len(json_ob[i]) else [])
+                else:
+                    task[i] = json_ob[i]
+            app_json_list.append(task)
+        self.__app_json = json.dumps(dict(devices=app_json_list))
+        return self.__app_json
+
+    def generate_package_json(self): # generate the package.json
+        command_list = json.loads(self.__command_json)
+        k = {
+            "name": "app",
+            "version": "0.1.0",
+            "description ": "a test",
+            "author": "ZekangLi",
+            "main": "src/index.js",
+            "ruff": {
+                "boards": {
+                    " ruff-mbd-v1 ": "^4.2.2",
+                    " *": "*"
+                    },
+                "dependencies": {}
+            }
+        }
+        for i in [task['driver'] for task in command_list]:
+            with open(self.path+'%s/package.json'%i, 'r+') as fp:
+                package = json.loads(fp.read())
+                k['ruff']["dependencies"][i] = "^" + package['version']
+        self.__package_json = json.dumps(k)
+        return self.__package_json
